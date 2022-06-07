63ea3be25607795a00ad45141e210f19d9302a4e
yyangso <soyeon0704@naver.com>
2022-06-07 15:17:18 +0900

    meebit

---
+++ smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,35 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import { ethereumTestnet, ethereumMainnet } from '../lib/Networks';
+import { openSea } from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: ethereumTestnet,
+  mainnet: ethereumMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'VeryCoolApes',
+  tokenName: 'Very Cool',
+  tokenSymbol: 'VCA',
+  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
+  maxSupply: 20,
+  whitelistSale: {
+    price: 0.05,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.07,
+    maxMintAmountPerTx: 2,
+  },
+  publicSale: {
+    price: 0.09,
+    maxMintAmountPerTx: 5,
+  },
+  contractAddress: "0xE788FbcDcd14aD8d92626fF984234745A941A5e4",
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: openSea,
+  whitelistAddresses: whitelistAddresses,
+};
+
+export default CollectionConfig;
